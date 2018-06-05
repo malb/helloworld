48861ba0fbb3050a4d81f6086bdfe5631e7f2b60
Martin R. Albrecht <martinralbrecht@googlemail.com>
2018-06-05 12:45:41 +0100

    PEP8, docstrings, doctests, shebang

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,27 @@
-print("Hello World!")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Hello World.
+
+.. moduleauthor:: Sam T. Jahn <samtjahn@gmail.com>
+
+.. note::
+
+   Run doctests as: ``python -m doctest helloworld.py``
+
+"""
+from __future__ import print_function
+
+
+def main():
+    """
+    Print 'Hello World!'
+
+    >>> main()
+    Hello World!
+
+    """
+    print("Hello World!")
+
+if __name__ == '__main__':
+    main()
